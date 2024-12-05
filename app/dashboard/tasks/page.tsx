9eb613d1c3f6e081616777229f2f8a218c92850c
Isola Pelumi <pelumiisola87@gmail.com>
2024-12-05 15:52:12 +0100

    feat: dynamically user org

--- app/dashboard/tasks/page.tsx
+++ app/dashboard/tasks/page.tsx
@@ -1,7 +1,12 @@
+"use client";
 import { Kanban } from "@/components/dashboard/home/Kanban";
 import DashboardLayout from "@/layouts/dasboard";
+import useUserStore from "@/store/userStore";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 export default function Tasks() {
+  const { user } = useUserStore();
   return (
     <DashboardLayout pageTitle="Tasks">
       <div className="w-full flex items-center justify-between md:pl-[260px] px-5 border py-2">
@@ -40,14 +45,17 @@ export default function Tasks() {
               </div>
             </div>
           </div>
+          {user?.organizations && (
+            <>
+              <div className="h-10 border"></div>
 
-          <div className="h-10 border"></div>
-
-          <div className="h-10 w-10 p-[1px] bg-white rounded-full z-20 flex items-center justify-center">
-            <div className="h-8 w-8 p-[1px] bg-gray-100 rounded-full flex items-center justify-center">
-              <i className="ri-add-line text-sm font-semibold"></i>
-            </div>
-          </div>
+              <div className="h-10 w-10 p-[1px] bg-white rounded-full z-20 flex items-center justify-center">
+                <div className="h-8 w-8 p-[1px] bg-gray-100 rounded-full flex items-center justify-center">
+                  <i className="ri-add-line text-sm font-semibold"></i>
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
       <div className="w-full md:pl-[260px] px-5 bg-neutral-100 min-h-screen overflow-x-scroll pr-5">
--- layouts/dasboard.tsx
+++ layouts/dasboard.tsx
@@ -1,45 +1,76 @@
 "use client";
-import { ReactNode, useEffect, useState } from "react";
-import Sidebar from "@/components/dashboard/Sidebar";
-import Navbar from "@/components/dashboard/Navbar";
-import useUserStore from "@/store/userStore";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import { useRouter } from "next/navigation";
+import Navbar from "@/components/dashboard/Navbar";
+import Sidebar from "@/components/dashboard/Sidebar";
+import Organization from "@/components/modals/Organization";
+import useUserStore from "@/store/userStore";
+import React, { ReactNode, ReactElement, useEffect, useState } from "react";
 
 type DashboardLayoutProps = {
   children: ReactNode;
-  pageTitle: String;
+  pageTitle: string;
 };
 
 export default function DashboardLayout({
   children,
   pageTitle,
 }: DashboardLayoutProps) {
-  const { user } = useUserStore(); // Access the user state
+  const { user } = useUserStore();
   const router = useRouter();
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    setIsMounted(true); // Mark the component as mounted
+    setIsMounted(true);
   }, []);
 
   useEffect(() => {
     if (isMounted && !user) {
-      router.push("/auth/sign-in"); // Redirect only if user is not logged in after mounting
+      router.push("/auth/sign-in");
     }
-  }, [isMounted, user]);
+  }, [isMounted, user, router]);
 
-  if (!isMounted || !user) {
-    // Show a loading state until the component is mounted and user is confirmed
+  const getUserOrg = async (userId: string) => {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_APP_BASE_URL}/members/user/${userId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${user?.token}`,
+        },
+      }
+    );
+    return response.data;
+  };
+
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ["userOrg", user?.id],
+    queryFn: () => getUserOrg(user!.id),
+    enabled: !!user?.id,
+    retry: true,
+  });
+
+  if (!isMounted) {
     return <div>Loading...</div>;
   }
+
+  const childWithProps = React.isValidElement(children)
+    ? React.cloneElement(children as ReactElement, { organizationData: data })
+    : children;
 
   return (
     <main className="w-full">
       <div className="relative w-full">
         <Sidebar />
         <Navbar pageTitle={pageTitle} />
-        <div className="w-full">{children}</div>
+        <div className="w-full">{childWithProps}</div>
       </div>
+      {user && !user.organizations && !data && (
+        <div className="bg-black flex items-center justify-center bg-opacity-60 w-screen h-screen fixed top-0 z-[1000]">
+          <Organization />
+        </div>
+      )}
     </main>
   );
 }
+
